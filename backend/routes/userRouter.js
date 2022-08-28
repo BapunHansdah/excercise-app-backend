eae2debe1b2ea3b841241ba48e33874eaa522626
Bapun <bapunhansdah777@gmail.com>
2022-08-28 17:31:20 +0530

    delete all

--- backend/routes/userRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const {authUser,registerUser} =  require('../controllers/userController.js')
-const router = express.Router()
-
-router.route('/').post(registerUser)
-router.post('/login',authUser)
-
-module.exports= router;
